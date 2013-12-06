9040e509991d793895092059a9576005abc6c0cb
Mike Mournighan <mjm2238@gmail.com>
2013-12-06 01:56:30 -0500

    just added opening of the dest files with preprocessor directives

--- hw2.c
+++ hw2.c
@@ -2,7 +2,12 @@
 #include "inject.h"
 #include <unistd.h>
 
+#define T_RD
+/*#define T_RDWR*/
+/*#define T_WR*/
 int main() {
+
+    // Make the file
     int i;
     char *src = "/data/local/tmp/test.txt";
     char *dest = "/data/local/tmp/test1.txt";
@@ -12,5 +17,23 @@ int main() {
         fprintf(stdout, "Current working dir: %s\n", cwd);
     else
         perror("getcwd() error");
+       
+    // Open the file     
+    FILE *fp;    
+#ifdef T_RD
+    fp = fopen(dest,"r");
+#elif defined T_RDWR
+    fp = fopen(dest, "rw");
+#elif defined T_WR
+    fp = fopen(dest, "w");
+#endif
+    if(fp == NULL)
+        printf("dest did not open\n");
+    else 
+    {
+        printf("file opened\n");
+        fclose(fp);    // Close
+    }
+            
     return 0;
 }
